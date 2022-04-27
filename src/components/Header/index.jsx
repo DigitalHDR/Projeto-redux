097d3f311ca0ab654664258d9170ed11e54e03b4
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-04-27 14:08:43 -0300

    estilizando o header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.svg'
+import './style.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="container">
+      <Link to="/">
+        <img className="logo" src={logo} alt="logo" />
+      </Link>
+
+      <Link className='reserva' to="/reservas">
+        <div>
+          <strong>Minhas Reservas</strong>
+          <span>0 reservas</span>
+        </div>
+      </Link>
+    </header>
   )
 }
 
