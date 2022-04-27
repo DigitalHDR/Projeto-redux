64fd0dc2d41ec0d5498fb22494e45ce9d583fb1c
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-04-27 12:33:17 -0300

    base do projeto (React Router v6)

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Reservas from './pages/Reservas'
+
+export default function Rotas() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="reservas" element={<Reservas />} />
+    </Routes>
+  )
+}
