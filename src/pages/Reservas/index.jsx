67b9374b116010b9f0994403335b65bade330b76
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-04-27 15:36:27 -0300

    estilizando reservas

--- src/pages/Reservas/index.jsx
+++ src/pages/Reservas/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './style.css'
+import { MdDelete } from 'react-icons/md'
 
 const Reservas = () => {
   return (
-    <div>Reservas</div>
+    <>
+      <div>Voce solicitou 1 Reservas</div>
+      <div className="reservas">
+        <img
+          src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
+          alt="maceio"
+        />
+        <strong>Viagem maceio 7 dias</strong>
+        <span>Quantidade: 2</span>
+        <button type="button" onClick={() => {}}>
+          <MdDelete size={20} color="#000" />
+        </button>
+      </div>
+    </>
   )
 }
 
