3561df84ba0130ed29d8410c393611cee484c54c
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-04-27 15:21:22 -0300

    estilizando o home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import api from '../../services/api'
+import { MdOutlineFlightTakeoff } from 'react-icons/md'
+import './style.css'
 
 const Home = () => {
   const [tripes, setTripes] = useState([])
@@ -7,14 +9,31 @@ const Home = () => {
   useEffect(() => {
     async function loadApi() {
       const response = await api.get('trips')
-      setTripes(response.data) //esse .data é para pegar todo que estiver em trip na api
-      console.log(response.data)
+      setTripes(response.data) //.data é para pegar todo que estiver em trip na api
     }
     loadApi()
   }, [])
 
+  return (
+    <>
+      <div className="box">
+        {tripes.map((trip, index) => (
+          <li key={index}>
+            <img src={trip.image} alt={trip.title} />
+            <strong>{trip.title}</strong>
+            <span>Status: {trip.status ? 'Disponivel' : 'Indisponível'}</span>
 
-  return <div>Home</div>
+            <button type="button" onClick={() => {}}>
+              <div>
+                <MdOutlineFlightTakeoff size={16} color="#fff" />
+              </div>
+              <span>Solicitar Reserva </span>
+            </button>
+          </li>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default Home
