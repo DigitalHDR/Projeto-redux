ffe04f262865ba3aa84959799aebceb1aa863271
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-04-27 14:40:02 -0300

    config api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import api from '../../services/api'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  const [tripes, setTripes] = useState([])
+
+  useEffect(() => {
+    async function loadApi() {
+      const response = await api.get('trips')
+      setTripes(response.data) //esse .data é para pegar todo que estiver em trip na api
+      console.log(response.data)
+    }
+    loadApi()
+  }, [])
+
+
+  return <div>Home</div>
 }
 
 export default Home
